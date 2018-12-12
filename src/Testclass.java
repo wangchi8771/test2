354e1e4f1c336f44d50e84d5595d9013d3b41a5a
wangchi8771 <1664977094@qq.com>
2018-12-12 14:32:01 +0800

    测试第一次提交

---
+++ src/Testclass.java
@@ -0,0 +1,14 @@
+
+public class Testclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Github测试成功");
+		System.out.println("Github测试成功");
+		System.out.println("Github测试成功");
+		System.out.println("Github测试成功");
+		
+
+	}
+
+}
